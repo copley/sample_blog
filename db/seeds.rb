1eea42e775cda17e006cfe32c75009a5f6977be7
Max Copley <max.copley@loyalty.co.nz>
2017-01-06 11:49:43 +1300

    Add debugging gem pry

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-#
-
-user = User.create :email => 'mary@example.com', :password => 'guessit'
+user = User.create :email => '1@example.com', :password => '1'
 Category.create [{:name => 'Programming'},
   {:name => 'Event'},
   {:name => 'Travel'},
   {:name => 'Music'},
   {:name => 'TV'}]
 user = User.create :email => 'passwordispassword@example.com', :password => 'password'
-Category.create [{:name => 'Sample Name Here'},
-  {:name => 'Event'},
-  {:name => 'Travel'}, 
-  {:name => 'Music'},
-  {:name => 'TV'}]
+article = Article.create :title => 'hhhhhhhhh', :body => 'Lorem With the
+file db/seeds.rb, the Rails gods have given us a way of feeding default
+values easily and quickly to a fresh installation. This is a normal Ruby
+program within the Rails environment. You have full access to all classes
+and methods of your application.
+So you do not need to enter everything manually with rails console in ord
+  er to make the records created in the section called “create” available
+  in a new Rails application, but you can simply use the following file db
+  /seeds.rb:'
