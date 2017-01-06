e08fb51fca9bfa06800ba633c25e4cff06ee749c
Max Copley <max.copley@loyalty.co.nz>
2017-01-06 17:36:23 +1300

    Changes to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,74 @@
-user = User.create :email => '1@example.com', :password => '1'
+user = User.create :email => '1@example.com', :password => ''
 Category.create [{:name => 'Programming'},
   {:name => 'Event'},
   {:name => 'Travel'},
   {:name => 'Music'},
   {:name => 'TV'}]
 user = User.create :email => 'passwordispassword@example.com', :password => 'password'
+
+article = Article.create :title => 'hhhhhhhhh', :body => 'Lorem With the
+file db/seeds.rb, the Rails gods have given us a way of feeding default
+values easily and quickly to a fresh installation. This is a normal Ruby
+program within the Rails environment. You have full access to all classes
+and methods of your application.
+So you do not need to enter everything manually with rails console in ord
+  er to make the records created in the section called “create” available
+  in a new Rails application, but you can simply use the following file db
+  /seeds.rb:'
+article = Article.create :title => 'hhhhhhhhh', :body => 'Lorem With the
+file db/seeds.rb, the Rails gods have given us a way of feeding default
+values easily and quickly to a fresh installation. This is a normal Ruby
+program within the Rails environment. You have full access to all classes
+and methods of your application.
+So you do not need to enter everything manually with rails console in ord
+  er to make the records created in the section called “create” available
+  in a new Rails application, but you can simply use the following file db
+  /seeds.rb:'
+article = Article.create :title => 'hhhhhhhhh', :body => 'Lorem With the
+file db/seeds.rb, the Rails gods have given us a way of feeding default
+values easily and quickly to a fresh installation. This is a normal Ruby
+program within the Rails environment. You have full access to all classes
+and methods of your application.
+So you do not need to enter everything manually with rails console in ord
+  er to make the records created in the section called “create” available
+  in a new Rails application, but you can simply use the following file db
+  /seeds.rb:'
+article = Article.create :title => 'hhhhhhhhh', :body => 'Lorem With the
+file db/seeds.rb, the Rails gods have given us a way of feeding default
+values easily and quickly to a fresh installation. This is a normal Ruby
+program within the Rails environment. You have full access to all classes
+and methods of your application.
+So you do not need to enter everything manually with rails console in ord
+  er to make the records created in the section called “create” available
+  in a new Rails application, but you can simply use the following file db
+  /seeds.rb:'
+article = Article.create :title => 'hhhhhhhhh', :body => 'Lorem With the
+file db/seeds.rb, the Rails gods have given us a way of feeding default
+values easily and quickly to a fresh installation. This is a normal Ruby
+program within the Rails environment. You have full access to all classes
+and methods of your application.
+So you do not need to enter everything manually with rails console in ord
+  er to make the records created in the section called “create” available
+  in a new Rails application, but you can simply use the following file db
+  /seeds.rb:'
+article = Article.create :title => 'hhhhhhhhh', :body => 'Lorem With the
+file db/seeds.rb, the Rails gods have given us a way of feeding default
+values easily and quickly to a fresh installation. This is a normal Ruby
+program within the Rails environment. You have full access to all classes
+and methods of your application.
+So you do not need to enter everything manually with rails console in ord
+  er to make the records created in the section called “create” available
+  in a new Rails application, but you can simply use the following file db
+  /seeds.rb:'
+article = Article.create :title => 'hhhhhhhhh', :body => 'Lorem With the
+file db/seeds.rb, the Rails gods have given us a way of feeding default
+values easily and quickly to a fresh installation. This is a normal Ruby
+program within the Rails environment. You have full access to all classes
+and methods of your application.
+So you do not need to enter everything manually with rails console in ord
+  er to make the records created in the section called “create” available
+  in a new Rails application, but you can simply use the following file db
+  /seeds.rb:'
 article = Article.create :title => 'hhhhhhhhh', :body => 'Lorem With the
 file db/seeds.rb, the Rails gods have given us a way of feeding default
 values easily and quickly to a fresh installation. This is a normal Ruby
